d2d76ff02e2a1cc13156af50327dcf9d16ef312c
AndreyUA <infernalsteel666@gmail.com>
2025-10-21 11:16:34 +0300

    30 - request obj undertanding

--- 01.nodejs-first-server/server.js
+++ 01.nodejs-first-server/server.js
@@ -1,7 +1,8 @@
 const http = require("http");
 
 const reqListener = (req, res) => {
-  console.log("req >> ", req);
+  //   console.log("req >> ", req);
+  console.log("req >> ", req.url, req.headers);
 
   // process.exit(); to stop node.js code execution manually
 };
