c39fe3910b5f80b2112fd53fbc576cb24eada243
AndreyUA <infernalsteel666@gmail.com>
2025-10-21 11:10:28 +0300

    28 - understanding of lifecycle and event loop

--- 01.nodejs-first-server/server.js
+++ 01.nodejs-first-server/server.js
@@ -2,6 +2,8 @@ const http = require("http");
 
 const reqListener = (req, res) => {
   console.log("req >> ", req);
+
+  // process.exit(); to stop node.js code execution manually
 };
 
 const server = http.createServer(reqListener);
