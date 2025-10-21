ca7f609c8737a236ef9a861cb47b9ea92a7d7e55
AndreyUA <infernalsteel666@gmail.com>
2025-10-21 11:24:32 +0300

    31 - response obj understanding

--- 01.nodejs-first-server/server.js
+++ 01.nodejs-first-server/server.js
@@ -5,6 +5,13 @@ const reqListener = (req, res) => {
   console.log("req >> ", req.url, req.headers);
 
   // process.exit(); to stop node.js code execution manually
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My first page</title></head>");
+  res.write("<body><h1>Hello from node.js!</h1></body>");
+  res.write("</html>");
+  res.end();
 };
 
 const server = http.createServer(reqListener);
