fc5169ba3a1829f1878366a05823ebb843270c36
AndreyUA <infernalsteel666@gmail.com>
2025-10-21 11:33:33 +0300

    33 - basic routing

--- 01.nodejs-first-server/server.js
+++ 01.nodejs-first-server/server.js
@@ -2,11 +2,23 @@ const http = require("http");
 
 const reqListener = (req, res) => {
   //   console.log("req >> ", req);
-  console.log("req >> ", req.url, req.headers);
-
+  //   console.log("req >> ", req.url, req.headers);
   // process.exit(); to stop node.js code execution manually
 
+  const url = req.url;
+
   res.setHeader("Content-Type", "text/html");
+
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter message</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
   res.write("<html>");
   res.write("<head><title>My first page</title></head>");
   res.write("<body><h1>Hello from node.js!</h1></body>");
