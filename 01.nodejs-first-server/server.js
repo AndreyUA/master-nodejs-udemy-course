2652fd3de29a7b7c5140c3ff224944656fd48095
AndreyUA <infernalsteel666@gmail.com>
2025-10-21 12:07:11 +0300

    35 - request body parsing

--- 01.nodejs-first-server/server.js
+++ 01.nodejs-first-server/server.js
@@ -17,7 +17,19 @@ const reqListener = (req, res) => {
   }
 
   if (url === "/message" && method === "POST") {
-    fs.writeFileSync("message.txt", "TEST TEXT");
+    const body = [];
+
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")?.[1] ?? "";
+
+      fs.writeFileSync("message.txt", message);
+    });
+
     res.writeHead(302, { Location: "/" });
 
     return res.end();
