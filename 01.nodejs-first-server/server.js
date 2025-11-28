9739ce4b2dfa075d63f3bcd93c8da7fe6f3ff37a
AndreyUA <infernalsteel666@gmail.com>
2025-11-28 15:04:27 +0200

    39 - node.js module for code splitting

--- 01.nodejs-first-server/server.js
+++ 01.nodejs-first-server/server.js
@@ -1,48 +1,7 @@
 const http = require("http");
-const fs = require("fs");
 
-const reqListener = (req, res) => {
-  const url = req.url;
-  const method = req.method;
+const { requestHandler } = require("./routes");
 
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Enter message</title></head>");
-    res.write(
-      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>"
-    );
-    res.write("</html>");
-
-    return res.end();
-  }
-
-  if (url === "/message" && method === "POST") {
-    const body = [];
-
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
-
-    return req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")?.[1] ?? "";
-
-      fs.writeFile("message.txt", message, (_err) => {
-        res.writeHead(302, { Location: "/" });
-
-        return res.end();
-      });
-    });
-  }
-
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My first page</title></head>");
-  res.write("<body><h1>Hello from node.js!</h1></body>");
-  res.write("</html>");
-  res.end();
-};
-
-const server = http.createServer(reqListener);
+const server = http.createServer(requestHandler);
 
 server.listen(3000);
