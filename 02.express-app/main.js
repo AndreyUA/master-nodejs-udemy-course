3b3fb1b0bcac22ea89b2026bee2e9ad2b35b3c78
AndreyUA <infernalsteel666@gmail.com>
2026-01-26 17:37:38 +0200

    61 - basic middlewares

--- 02.express-app/main.js
+++ 02.express-app/main.js
@@ -4,6 +4,19 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("we are in the middleware!");
+
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("we are in the 2nd middleware!");
+
+  // TODO: this won't allow request to "move further"
+  //   next();
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
