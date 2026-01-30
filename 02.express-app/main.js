562b8a404f98d8a49e26cea1316bfa161426dc7e
AndreyUA <infernalsteel666@gmail.com>
2026-01-30 11:47:23 +0200

    add different routes and one middleware

--- 02.express-app/main.js
+++ 02.express-app/main.js
@@ -4,13 +4,18 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("we are in the middleware!");
-
+app.use("/", (req, res, next) => {
+  console.log("this is the first middleware. It always runs.");
   next();
 });
 
-app.use((req, res, next) => {
+app.use("/add-product", (req, res, next) => {
+  console.log("we are in the 2nd middleware!");
+
+  res.send("<p>add product page</p>");
+});
+
+app.use("/", (req, res, next) => {
   console.log("we are in the 2nd middleware!");
 
   res.send("<p>hello from Express</p>");
