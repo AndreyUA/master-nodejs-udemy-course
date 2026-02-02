59161a773b793f5582a9ad78efc436312062a70b
AndreyUA <infernalsteel666@gmail.com>
2026-02-02 14:53:20 +0200

    71 - add serving of html pages

--- 02.express-app/main.js
+++ 02.express-app/main.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -12,7 +14,7 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use("/", (_req, res, _next) => {
-  res.sendStatus(404).send("<h1>Page not found</h1>");
+  res.sendFile(path.join(__dirname, "/views/not-found.html"));
 });
 
 app.listen(3_000);
