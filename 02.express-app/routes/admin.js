9f3730d5b05f2e767507768d0bc9f9479b001263
AndreyUA <infernalsteel666@gmail.com>
2026-02-02 12:51:36 +0200

    68 - filtering paths with app.use on top level

--- 02.express-app/routes/admin.js
+++ 02.express-app/routes/admin.js
@@ -4,11 +4,11 @@ const router = express.Router();
 
 router.get("/add-product", (_req, res, _next) => {
   res.send(
-    "<form action='/product' method='post'><input type='text' name='title'><button type='submit'>Add product</button></form>",
+    "<form action='/admin/add-product' method='post'><input type='text' name='title'><button type='submit'>Add product</button></form>",
   );
 });
 
-router.post("/product", (req, res, _next) => {
+router.post("/add-product", (req, res, _next) => {
   console.log("request body: ", req.body);
 
   res.redirect("/");
